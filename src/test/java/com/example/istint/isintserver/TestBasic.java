34c1dcf8a8424d1d00d9e45772b3dc2bfc431b96
mpapenbr <Markus.Papenbrock@gmail.com>
2020-08-16 19:14:13 +0000

    play around with the mongodb/devcontainer stuff

---
+++ src/test/java/com/example/istint/isintserver/TestBasic.java
@@ -0,0 +1,24 @@
+package com.example.istint.isintserver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+public class TestBasic {
+    @DisplayName("Ich will nur testen!")
+    @Test
+    public void test() {
+        System.out.println("TestBasic.test()");
+    }
+}
