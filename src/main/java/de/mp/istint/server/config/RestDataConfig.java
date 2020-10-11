64dfd8b81bac53ab3dff51b59e381bc429a43096
mpapenbr <Markus.Papenbrock@gmail.com>
2020-10-11 09:10:42 +0200

    Comment

--- src/main/java/de/mp/istint/server/config/RestDataConfig.java
+++ src/main/java/de/mp/istint/server/config/RestDataConfig.java
@@ -7,11 +7,16 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import de.mp.istint.server.model.Event;
 import de.mp.istint.server.model.User;
 
+/**
+ * This configuration is used to configure what is included in auto-generated REST-responses (by
+ * spring)
+ */
 @Configuration
 public class RestDataConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        // we want Ids for Event and User
         config.exposeIdsFor(Event.class, User.class);
     }
 }
